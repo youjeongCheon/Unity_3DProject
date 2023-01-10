2b178a98302d52ebd0c3a1c75713cd25fe9ece4b
youjeongCheon <115628032+youjeongCheon@users.noreply.github.com>
2023-01-10 16:18:33 +0900

    feat : Joint prefab 추가

---
+++ Unity_3DProject/Assets/Scripts/Custom/FixedJoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedJoint : MonoBehaviour
+{
+    private Rigidbody connectedBody;
+    private void Awake()
+    {
+        
+    }
+}
