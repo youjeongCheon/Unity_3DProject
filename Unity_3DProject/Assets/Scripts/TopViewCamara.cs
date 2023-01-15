28dc9ccbad3c397580f24ffbc2864ae5f67c818c
youjeongCheon <115628032+youjeongCheon@users.noreply.github.com>
2023-01-16 03:24:01 +0900

    feat : Game TopView Camera 이동, 범위 제한 구현

---
+++ Unity_3DProject/Assets/Scripts/TopViewCamara.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopViewCamara : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 min;
+    [SerializeField]
+    private Vector3 max;
+    [SerializeField]
+    private float moveTime=0.2f;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        Vector3 newCamPos = target.position;
+        newCamPos.x = Mathf.Clamp(newCamPos.x, min.x, max.x);
+        newCamPos.y = transform.position.y;
+        newCamPos.z = Mathf.Clamp(newCamPos.z, min.z, max.z);
+        this.transform.position = Vector3.Lerp(transform.position, newCamPos, moveTime);
+    }
+
+}
